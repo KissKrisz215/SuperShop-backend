38a50667f1eedd027280d3426d8c57d71ac0e47d
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-11-06 15:53:15 +0100

    Setup Express and added User Schema

---
+++ model/User.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+    lowercase: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
+
+const User = model("User", userSchema);
+module.exports = User;
