bd7f4166e5fcbaf5af050d4638baf819590c3d75
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-11-06 16:14:14 +0100

    Added Signup Route

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,45 @@
 const express = require("express");
-
+const User = require("../model/User.model");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("POST API");
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!email.match(emailRegex)) {
+      return res.status(400).json({
+        message: "Email must be a valid email address.",
+      });
+    }
+
+    if (!password.match(passwordRegex)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters long and contain at least one special character.",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
