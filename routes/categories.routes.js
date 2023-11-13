645f46c87a489546e6f3700bd90d32a5fa46b4da
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-11-13 14:07:59 +0100

    Modified Categories :id route to also search in children array

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Category = require("../model/Categories.model");
+const Product = require("../model/Products.model");
 
 router.get("/all", async (req, res) => {
   try {
@@ -15,19 +16,22 @@ router.get("/:id", async (req, res) => {
   const categoryId = req.params.id;
 
   try {
-    const allCategories = await Category.find({}).populate("children").exec();
-    const category = allCategories.find(
-      (cat) => cat._id.toString() === categoryId
-    );
+    const category = await Category.findOne({
+      $or: [{ _id: categoryId }, { "children._id": categoryId }],
+    })
+      .populate("children")
+      .exec();
 
-    if (category) {
-      res.send(category);
-    } else {
-      res.status(404).send("Category not found");
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
     }
+
+    const productsInCategory = await Product.find({ categories: categoryId });
+
+    res.send(productsInCategory);
   } catch (error) {
-    console.error("Error fetching categories:", error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error fetching category and associated products:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
